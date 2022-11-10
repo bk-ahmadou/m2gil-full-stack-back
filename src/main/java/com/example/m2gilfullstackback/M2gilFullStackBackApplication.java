72fc9839043c82bedc636ed4758c6d4a6eb79a58
bk.ahmadou <ahmadoukassoum7@gmail.com>
2022-11-11 00:19:42 +0100

    Initial commit

---
+++ src/main/java/com/example/m2gilfullstackback/M2gilFullStackBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.m2gilfullstackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2gilFullStackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2gilFullStackBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/m2gilfullstackback/M2gilFullStackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m2gilfullstackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2gilFullStackBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
